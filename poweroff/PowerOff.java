63d644edd7200b14538a358a9794a8a58b00e20f
kobearv <arvindbaskar24@gmail.com>
2017-11-15 20:38:09 +0530

    Update PowerOff.java

--- poweroff/PowerOff.java
+++ poweroff/PowerOff.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package poweroff;
 
 import mousekeyboardcontrol.MouseKeyboardControl;
 
-/**
- *
- * @author varun
- */
 public class PowerOff {
     String os;
     Runtime runtime;
